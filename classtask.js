e92465f6258a690283b219f4b763f37d294d1407
toseef39 <toseefkalwar2001@gmail.com>
2024-09-13 19:35:21 +0500

    conditionaltask

---
+++ classtask.js
@@ -0,0 +1,14 @@
+let Marks = +prompt("What is your Grade") ;
+   
+if (Marks > 79 && Marks <= 100){
+    alert("A1 Grade ")
+}
+else if (Marks > 69 && Marks <= 79){
+     alert(" A Grade ")
+}
+else if (Marks > 59 &&  Marks  <= 69){
+    alert(" B Grade ")
+}
+else {
+    alert(" fail")
+}
